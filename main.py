bbb7481c02d1362fe343b795e9b646f3f661c452
Max Netyaga <max.netyaga@gmail.com>
2025-12-14 18:37:18 +0200

    broadcast & unicast messages work. better logging to destinguish different workers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import logging
 import os
 import tempfile
@@ -7,6 +8,7 @@ from contextlib import asynccontextmanager
 from pathlib import Path
 from typing import Annotated
 
+import psutil
 from fastapi import (
     Body,
     Depends,
@@ -19,10 +21,58 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.websockets import WebSocketState
 from starlette.websockets import WebSocketDisconnect
 
-logger = logging.getLogger("uvicorn")
+COLORS = [
+    "\033[31m",  # red
+    "\033[32m",  # green
+    "\033[33m",  # yellow
+    "\033[34m",  # blue
+    "\033[35m",  # magenta
+    "\033[36m",  # cyan
+    "\033[91m",  # bright red
+    "\033[92m",  # bright green
+    "\033[93m",  # bright yellow
+    "\033[94m",  # bright blue
+    "\033[95m",  # bright magenta
+    "\033[96m",  # bright cyan
+    "\033[37m",  # white
+    "\033[90m",  # bright black (gray)
+    "\033[97m",  # bright white
+    "\033[38;5;208m",  # orange
+    "\033[38;5;214m",  # light orange
+    "\033[38;5;178m",  # gold
+    "\033[38;5;118m",  # lime
+    "\033[38;5;81m",  # sky blue
+    "\033[38;5;135m",  # purple
+    "\033[38;5;141m",  # lavender
+    "\033[38;5;203m",  # pink
+]
+RESET = "\033[0m"
 
-tempdir = Path(tempfile.gettempdir()) / "netyaga-test-task-2025-12-14"
-tempdir.mkdir(exist_ok=True)
+
+class ProcessColorFormatter(logging.Formatter):
+    def format(self, record):
+        pid = record.process
+        assert pid
+        color = COLORS[pid % len(COLORS)]
+
+        record.msg = f"{color}{record.msg}{RESET}"
+        return super().format(record)
+
+
+logger = logging.getLogger("uvicorn")
+for handler in logger.handlers:
+    handler.setFormatter(
+        ProcessColorFormatter(
+            "%(asctime)s | PID=%(process)s | %(levelname)s | %(filename)s:%(lineno)d | %(message)s"
+        )
+    )
+
+tempdir = (
+    Path(tempfile.gettempdir())
+    / "netyaga-test-task-2025-12-14"
+    / datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%d%m%Y%H%M%S")
+)
+tempdir.mkdir(exist_ok=True, parents=True)
 
 # load_dotenv(dotenv_path="./.env")
 port = os.environ.get("PORT") or 12696
@@ -42,26 +92,28 @@ async def _local_socket_handle(
     reader: asyncio.StreamReader,
     writer: asyncio.StreamWriter,
 ):
-    addr = writer.get_extra_info("peername")
-    print("Connected:", addr)
-
     # wire protocol: first 128 bits are id, then message (in utf-8) till eof
-    client_id_bin = await reader.read(8)
+    client_id_bin = await reader.read(16)
     msg_bin = await reader.read()
     # 0 corresponds to broadcast
     # multicast is not implemented
     client_id = uuid.UUID(int=int.from_bytes(client_id_bin))
 
-    if client_id == 0:
+    if client_id.int == 0:
+        logger.info("Retranslating broadcast message to all websocket connections...")
         for ws in connections.values():
-            await ws.send_json(msg_bin.decode())
-
+            msg = msg_bin.decode()
+            await ws.send_json(msg)
+        logger.info("Retranslated to %s clients", len(connections))
+    elif client_id not in connections:
+        return
     else:
         await connections[client_id].send_json(msg_bin.decode())
 
 
 async def _listen_local_socket(local_socket_path: Path):
     """Listens worker exclusive socket"""
+    logger.info("Listening unix socket: %s", local_socket_path)
     server = await asyncio.start_unix_server(
         _local_socket_handle,
         path=local_socket_path,
@@ -78,6 +130,8 @@ async def lifespan(app: FastAPI):
     )
 
     yield
+
+    local_socket_path.unlink()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -99,7 +153,7 @@ async def websocket_endpoint(websocket: WebSocket):
                 await websocket.receive_text()
                 # Not implemented
 
-        await asyncio.gather(listen())
+        await listen()
 
         await websocket.close()
     except WebSocketDisconnect:
@@ -129,7 +183,7 @@ async def send(
 
         client_id = client_id or uuid.UUID(int=0)
 
-        writer.write(int(client_id).to_bytes(length=128))
+        writer.write(int(client_id).to_bytes(length=16))
         writer.write(msg.encode())
 
         await writer.drain()
